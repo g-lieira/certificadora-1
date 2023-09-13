56e47108bae5e29f6649e1cea8b90b3cf7192773
Talita Aya <tatakuriki@gmail.com>
2023-09-13 13:55:19 -0300

    tela de login com inputs, botão e imagem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
+import Login from './login/login'
+
 function App() {
   return (
     <div className="App">
-        <h1>Hello World</h1>
+        <Login />
       </div>
   );
 }
---
+++ src/login/login.js
@@ -0,0 +1,83 @@
+import * as React from "react";
+import { Button, TextField, styled } from "@mui/material";
+
+import "./login.css";
+
+import loginImg from "../assets/login-img.svg";
+
+const Input = styled(TextField)`
+  & label {
+    color: #9E7D9E;
+    backgroundColor: red;
+  }
+
+  & label.Mui-focused {
+    color: #9E7D9E;
+  }
+
+  & .MuiOutlinedInput-root {
+    color: #9E7D9E;
+    & fieldset {
+      border-color: #9E7D9E;
+    }
+    &:hover fieldset {
+      border-color: #9E7D9E;
+    }
+    &.Mui-focused fieldset {
+      border-color: #9E7D9E;
+    }
+  }
+`
+
+function Login() {
+  return (
+    <div className="login-container">
+      <h1>Bem-vindo!</h1>
+      <img
+        src={loginImg}
+        alt="ilustração de um fastama, utilizado como avatar"
+      />
+      <div className="login-info">
+        <Input
+          fullWidth
+          id="outlined-basic"
+          label="Nome"
+          variant="outlined"
+          margin="normal"
+        />
+        <Input
+          fullWidth
+          id="outlined-basic"
+          label="E-mail"
+          variant="outlined"
+          margin="normal"
+        />
+        <Button 
+          variant="contained" 
+          sx={[
+            { 
+            borderRadius: '50px',
+            width: 3/4,
+            height: '50px',
+            margin: '25px 0',
+            fontFamily: 'Lato', 
+            fontSize: '18px',
+            fontWeight: '800',
+            backgroundColor: '#6997DC',
+            color: '#202020',
+            letterSpacing: '5px',
+
+            ":hover": {
+              backgroundColor: "#47B39D",
+            }
+            }
+          ]}
+        >
+          Entrar
+        </Button>
+      </div>
+    </div>
+  );
+}
+
+export default Login;
