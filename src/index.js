981ca466fe7f6c6be9dbe7e5b40fb7af6c12e462
Talita Aya <tatakuriki@gmail.com>
2023-11-16 07:40:32 -0300

    estilização corrigida

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App/App';
 import AuthProvider from './App/Context/AuthContext';
 
+import "./index.scss";
+
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
